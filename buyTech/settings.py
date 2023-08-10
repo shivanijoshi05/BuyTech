18edc3d2b105ca4d3a24831c65d4be0d7c236322
shivani <shivanijoshi075@gmail.com>
2023-08-10 13:08:45 +0530

    fic: superuser settings change

--- buyTech/settings.py
+++ buyTech/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&)oj^n^0eir6zl(tocx_0=$(wz*9+$!dhu(pzq1epc&j+lwsdo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
