c27cf650f6c5f580c2bd01628fea87543daeb094
shivani <shivanijoshi075@gmail.com>
2023-08-11 12:34:05 +0530

    update production and development settings

--- buyTech/dev.py
+++ buyTech/dev.py
@@ -12,6 +12,21 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': ['templates',],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 # Base url to serve media files
 MEDIA_URL = "/media/"
--- buyTech/production.py
+++ buyTech/production.py
@@ -5,6 +5,22 @@ DEBUG = False
 STATIC_URL = '/static/'
 STATIC_ROOT =  '/home/shivanijoshi05/buyTech/static'
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': ['/home/shivanijoshi05/buyTech/templates',],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 
 # Base url to serve media files
 MEDIA_URL = "/media/"
