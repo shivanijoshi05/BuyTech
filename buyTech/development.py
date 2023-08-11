8c2e6e31cc974d196b356edc6fb4275f41fde7e7
shivani <shivanijoshi075@gmail.com>
2023-08-11 17:24:59 +0530

    fix: few settings

--- buyTech/development.py
+++ buyTech/development.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&)oj^n^0eir6zl(tocx_0=$(wz*9+$!dhu(pzq1epc&j+lwsdo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
